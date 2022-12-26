8275c9d6f53b580061f415a5598b0fd35732927a
parkseyeon06 <psyvitamin@gmail.com>
2022-12-26 11:50:42 +0900

    [grammar]switch 적용 후

--- GRAMMAR/switch.c
+++ GRAMMAR/switch.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 void main() {
 	int a = 1;
-	if (a == 1)
-		printf("apple");
-	else if (a == 2)
-		printf("banana");
-	else
-		printf("mango");
+	//a값이 정수로 딱 하나만 떨어지는 경우 switch 이용
+	switch (a) {
+	case 1:
+		printf("apple");break;
+	case 2:
+		printf("banana");break;
+	default:
+		printf("mango");break;
+	}
 	return 0;
 }
