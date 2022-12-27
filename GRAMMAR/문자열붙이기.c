e7e7d0ca28aec485de45c94970f7b6d44260a321
parkseyeon06 <psyvitamin@gmail.com>
2022-12-27 11:18:56 +0900

    문자열 덧붙이기 마지막

--- GRAMMAR/문자열붙이기.c
+++ GRAMMAR/문자열붙이기.c
@@ -3,13 +3,9 @@
 #include<stdlib.h>
 int main(void)
 {
-	char str[30] = "score : ";
+	char str[30];
 	int score = 300;
-	char str2[30];
-	//정수를 문자열로(변환할 정수,적용대상,진수)
-	itoa(score, str2, 10);
-
-	stract(str, str2);
+	sprintf(str, "score : %d", score);
 	printf("%s", str);
 
 	return 0;
